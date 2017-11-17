3ff9aea47cabc8c239b895d9d2925e9e09a7ae06
M Haidar Hanif <git@mhaidarhanif.com>
2017-11-17 16:50:56 +0700

    Add helper to decode token

--- routes/helpers/index.js
+++ routes/helpers/index.js
@@ -1,1 +1,10 @@
-module.exports = {}
+const jwt = require("jsonwebtoken")
+
+module.exports = {
+  // DECODE TOKEN
+  decodeToken: token => {
+    const decoded = jwt.verify(token, process.env.SECRET)
+    if (decoded) return decoded
+    else return null
+  }
+}
